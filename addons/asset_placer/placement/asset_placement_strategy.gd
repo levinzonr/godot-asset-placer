db78840abf477ae742a2cc2e372c329bb232756b
Flynsarmy <flynsarmy@gmail.com>
2025-09-06 11:23:40 +1000

    fix(#21): Don't error out if PlanePlacementStrategy ray intersect fails

--- addons/asset_placer/placement/asset_placement_strategy.gd
+++ addons/asset_placer/placement/asset_placement_strategy.gd
@@ -4,12 +4,13 @@ class_name AssetPlacementStrategy
 class CollisionHit:
 	var position: Vector3
 	var normal: Vector3
-	
+
 	func _init(position: Vector3, normal: Vector3):
 		self.position = position
 		self.normal = normal
 
-	static var ZERO := CollisionHit.new(Vector3.ZERO, Vector3.UP)
+	static func zero() -> CollisionHit:
+		return CollisionHit.new(Vector3.ZERO, Vector3.UP)
 
 func get_placement_point(camera: Camera3D, mouse_position: Vector2) -> CollisionHit:
-	return CollisionHit.ZERO
+	return CollisionHit.zero()
