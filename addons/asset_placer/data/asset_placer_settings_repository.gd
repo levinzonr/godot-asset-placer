af9540c172e2baafeed9302dda461fa8c3ccc3d5
Roman Levinzon <levinzon.roman@gmail.com>
2025-10-19 03:09:48 +0200

    feat: Use Project Settings as a source of truth for the Preview Material setting since the material can be absent in one project and present in another

--- addons/asset_placer/data/asset_placer_settings_repository.gd
+++ addons/asset_placer/data/asset_placer_settings_repository.gd
@@ -3,14 +3,15 @@ class_name AssetPlacerSettingsRepository
 
 signal settings_changed(settings: AssetPlacerSettings)
 
-var _settings: EditorSettings
+var _editor_settings: EditorSettings
 static var  instance: AssetPlacerSettingsRepository
 
 func _init():
-	_settings = EditorInterface.get_editor_settings()
+	_editor_settings = EditorInterface.get_editor_settings()
 	instance = self
 	
 	
+const KEY_BASE = "asset_placer/%s"	
 const KEY_BINDING_SCALE: String = "bindings/scale_asset"
 const KEY_BINDING_ROTATE: String = "bindings/rotate_asset"	
 const KEY_BINDING_TRANSLATE: String = "bindings/translate_asset"
@@ -23,7 +24,7 @@ func set_settings(settings: AssetPlacerSettings):
 	_set_editor_setting(KEY_BINDING_SCALE, settings.binding_scale)
 	_set_editor_setting(KEY_BINDING_TRANSLATE, settings.binding_translate)
 	_set_editor_setting(KEY_BINDING_GRID_SNAP, settings.binding_grid_snap)
-	_set_editor_setting(KEY_GENERAL_PREVIEW_MATERIAL, settings.preview_material_resource)
+	_set_project_setting(KEY_GENERAL_PREVIEW_MATERIAL, settings.preview_material_resource)
 	settings_changed.emit(get_settings())
 	
 func get_settings() -> AssetPlacerSettings:
@@ -32,16 +33,21 @@ func get_settings() -> AssetPlacerSettings:
 	settings.binding_translate = _get_editor_setting(KEY_BINDING_TRANSLATE, settings.binding_translate)
 	settings.binding_rotate = _get_editor_setting(KEY_BINDING_ROTATE, settings.binding_rotate)
 	settings.binding_grid_snap = _get_editor_setting(KEY_BINDING_GRID_SNAP, settings.binding_grid_snap)
-	settings.preview_material_resource = _get_editor_setting(KEY_GENERAL_PREVIEW_MATERIAL, settings.preview_material_resource)
+	settings.preview_material_resource = _get_project_setting(KEY_GENERAL_PREVIEW_MATERIAL, settings.preview_material_resource)
 	return settings
 		
 
+func _set_project_setting(key: String, value: Variant):
+	ProjectSettings.set_setting(KEY_BASE % key, value)
+	
+func _get_project_setting(key: String, default: Variant):
+	return ProjectSettings.get_setting(KEY_BASE % key, default)
 
 func _set_editor_setting(key: String, value):
-	_settings.set("asset_placer/%s" % key, value)
+	_editor_settings.set(KEY_BASE % key, value)
 
 func _get_editor_setting(key: String, default: Variant) -> Variant:
-	if _settings.has_setting("asset_placer/%s" % key):
-		return _settings.get("asset_placer/%s" % key)
+	if _editor_settings.has_setting(KEY_BASE % key):
+		return _editor_settings.get(KEY_BASE % key)
 	else:
 		return default
