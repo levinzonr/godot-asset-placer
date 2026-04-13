a9ce81563e9899580c9d0d63537888c857af37a6
Sr3qt <105977398+Sr3qt@users.noreply.github.com>
2026-04-13 06:07:47 +0200

    chore: rework reverse sorting functions
    This is better, because each function can individually handle a reverse sort.

--- addons/asset_placer/ui/asset_library_window/asset_sort_by.gd
+++ addons/asset_placer/ui/asset_library_window/asset_sort_by.gd
@@ -13,27 +13,29 @@ enum SortMethod {
 }
 
 
-static func sort_by_name(left: AssetResource, right: AssetResource) -> bool:
-	return left.name.naturalcasecmp_to(right.name) < 0
+static func sort_by_name(left: AssetResource, right: AssetResource, ascending_order := true) -> bool:
+	return not (ascending_order != (left.name.naturalnocasecmp_to(right.name) < 0))
 
 
-static func sort_by_date_added(left: AssetResource, right: AssetResource):
-	return left.date_added < right.date_added
+static func sort_by_date_added(
+	left: AssetResource, right: AssetResource, ascending_order := true
+) -> bool:
+	if ascending_order:
+		return left.date_added < right.date_added
+	return left.date_added > right.date_added
+
 
 
 static func get_sort_function(method: SortMethod, ascending_order := true) -> Callable:
 	var fun: Callable
 	match method:
 		SortMethod.Name:
-			fun = sort_by_name
+			fun = sort_by_name.bind(ascending_order)
 		SortMethod.DateAdded:
-			fun = sort_by_date_added
+			fun = sort_by_date_added.bind(ascending_order)
 		_:
 			push_warning("Chosen SortMethod %s is not supported." % method)
 			fun = _default_sort
-
-	if not ascending_order:
-		return func(left, right): return not fun.call(left, right)
 
 	return fun
 
