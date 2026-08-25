934b866bbe5100ef7e37292ba24d5a62c0a4b8f4
Roman Levinzon <33395420+levinzonr@users.noreply.github.com>
2026-08-25 15:46:55 -0400

    feat: Save asset placer options (#115)
    * refactor:  move setters from AssetPlacerPresenter to AssetPlacerOptions
    
    Also gave the AssetPlacerOptions properties more clear clear name.
    
    * feature: make AssetPlacerOptions load on start up and save on change
    
    * style: cleanup
    
    * chore: replace old options references
    
    * chore: make _loaded_options hot-swappable
    
    ---------
    
    Co-authored-by: Sr3qt <105977398+Sr3qt@users.noreply.github.com>

--- addons/asset_placer/utils/transformations.gd
+++ addons/asset_placer/utils/transformations.gd
@@ -10,14 +10,14 @@ static func apply_transforms(node: Node3D, options: AssetPlacerOptions, rng: Ran
 static func transform_rotation(
 	transform: Transform3D, options: AssetPlacerOptions, rng: RandomNumberGenerator
 ) -> Transform3D:
-	if not options.rotate_on_placement:
+	if not options.use_random_rotation:
 		return transform
 
-	var rx = rng.randf_range(options.min_rotation.x, options.max_rotation.x)
+	var rx = rng.randf_range(options.min_random_rotation.x, options.max_random_rotation.x)
 	transform = transform.rotated_local(Vector3(1, 0, 0), deg_to_rad(rx))
-	var ry = rng.randf_range(options.min_rotation.y, options.max_rotation.y)
+	var ry = rng.randf_range(options.min_random_rotation.y, options.max_random_rotation.y)
 	transform = transform.rotated_local(Vector3(0, 1, 0), deg_to_rad(ry))
-	var rz = rng.randf_range(options.min_rotation.z, options.max_rotation.z)
+	var rz = rng.randf_range(options.min_random_rotation.z, options.max_random_rotation.z)
 	transform = transform.rotated_local(Vector3(0, 0, 1), deg_to_rad(rz))
 	return transform
 
@@ -25,20 +25,20 @@ static func transform_rotation(
 static func transform_scale(
 	transform: Transform3D, options: AssetPlacerOptions, rng: RandomNumberGenerator
 ) -> Transform3D:
-	if not options.scale_on_placement:
+	if not options.use_random_scale:
 		return transform
 
-	if options.uniform_scaling:
-		var scale = rng.randf_range(options.min_scale.x, options.max_scale.x)
-		var basiz = transform.basis.orthonormalized().scaled(Vector3(scale, scale, scale))
-		transform.basis = basiz
+	if options.uniform_random_scaling:
+		var scale = rng.randf_range(options.min_random_scale.x, options.max_random_scale.x)
+		var basis = transform.basis.orthonormalized().scaled(Vector3(scale, scale, scale))
+		transform.basis = basis
 		return transform
 	else:
-		var scale_x = rng.randf_range(options.min_scale.x, options.max_scale.x)
-		var scale_y = rng.randf_range(options.min_scale.y, options.max_scale.y)
-		var scale_z = rng.randf_range(options.min_scale.z, options.max_scale.z)
-		var basiz = transform.basis.orthonormalized().scaled(Vector3(scale_x, scale_y, scale_z))
-		transform.basis = basiz
+		var scale_x = rng.randf_range(options.min_random_scale.x, options.max_random_scale.x)
+		var scale_y = rng.randf_range(options.min_random_scale.y, options.max_random_scale.y)
+		var scale_z = rng.randf_range(options.min_random_scale.z, options.max_random_scale.z)
+		var basis = transform.basis.orthonormalized().scaled(Vector3(scale_x, scale_y, scale_z))
+		transform.basis = basis
 		return transform
 
 
